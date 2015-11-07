0e2763fa80e04835360804b25906b5ab798097bd
accelerateusa <thefirebirdusa@yahoo.com>
2015-11-06 21:43:04 -0600

    First

---
+++ update_people_cats.py
@@ -0,0 +1,34 @@
+'''
+Created on Oct 25, 2014
+
+@author: David
+'''
+
+
+import shelve
+
+
+typesofpeople = ""
+
+typesofpeople = shelve.open("typesofpeople")
+typesofpeople["0"] = "test"
+
+print(typesofpeople["0"])
+
+
+print(range(5))
+
+top = [range(0,5)]
+print(top)
+
+
+
+if __name__ == '__main__':
+    for i in top:
+        typesofpeople[str(i)] = 0
+        print(typesofpeople[i])
+        
+
+
+
+typesofpeople.close()
